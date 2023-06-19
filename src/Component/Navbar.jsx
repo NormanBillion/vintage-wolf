b9d1422c4079214e34c9b63d27be2cab64a4026c
NormanBillion <norman.themba18@gmail.com>
2023-06-19 13:57:54 +0200

    add Pages and new files Contact, Wines, About

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -25,18 +25,19 @@ function Navbar() {
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    
+                                Dropdown
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Gallery</a></li>
+                                    <li><a className="dropdown-item" href="#">Shop</a></li>
+                                    <li><a className="dropdown-item" href="#">Events</a></li>
                                     <li><hr className="dropdown-divider" /></li>
                                     <li><a className="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
                             </li>
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <a className="nav-link disabled">Disabled</a>
-                            </li>
+                            </li> */}
                         </ul>
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
